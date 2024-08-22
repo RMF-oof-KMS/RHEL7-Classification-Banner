3191152aeb7291cee6a7c346b0321dfb1a7e959c
RMF-oof-KMS <116330452+RMF-oof-KMS@users.noreply.github.com>
2024-08-22 10:09:27 -0700

    Add KDE support (untested)

--- get-display.sh
+++ get-display.sh
@@ -3,7 +3,15 @@
 # Get the active user's session ID
 SESSION=$(loginctl | grep $(who | awk '{print $1}') | grep "seat0" | awk '{print $1}')
 
-# Get the display environment variable
-DISPLAY=$(loginctl show-session $SESSION -p Display | cut -d'=' -f2)
+# Determine the desktop environment (GNOME or KDE)
+DESKTOP_ENVIRONMENT=$(loginctl show-session $SESSION -p Type | cut -d'=' -f2)
+
+# Get the DISPLAY environment variable based on the desktop environment
+if [[ "$DESKTOP_ENVIRONMENT" == "x11" || "$DESKTOP_ENVIRONMENT" == "wayland" ]]; then
+    DISPLAY=$(loginctl show-session $SESSION -p Display | cut -d'=' -f2)
+else
+    # Fallback method for KDE, which may not provide a Display property
+    DISPLAY=$(ps -u $(whoami) -o pid=,cmd= | grep -E 'kdeinit5|kwin' | awk '{print ":0"}' | head -n1)
+fi
 
 echo $DISPLAY
